3169a1c75a8dc3c0c084fb147b7c81a9ae5c086c
Brendan Tobolaski <brendan@tobolaski.com>
2015-11-17 20:53:14 -0500

    Upgrade langohr

--- rabbitmq/project.clj
+++ rabbitmq/project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [jepsen "0.0.5"]
-                 [com.novemberain/langohr "2.7.1" ]])
+                 [com.novemberain/langohr "3.4.1" ]])
